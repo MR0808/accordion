d88e357784ded6c4191f05521b04f1bc8a36d126
MR0808 <kram@grebnesor.com>
2024-07-31 23:23:49 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import data from './data';
+import Questions from './Questions';
+
+const App = () => {
+    const [questions, setQuestions] = useState(data);
+    const [activeId, setActiveId] = useState(null);
+
+    const toggleQuestion = (id) => {
+        setActiveId(id === activeId ? null : id);
+    };
+
+    return (
+        <main>
+            <Questions
+                questions={questions}
+                activeId={activeId}
+                toggleQuestion={toggleQuestion}
+            />
+        </main>
+    );
+};
+export default App;
